22c753040d26a85eb7cbc0d7d75775862e9bc8f8
Arshath Farwyz <119394720+Arshath015@users.noreply.github.com>
2026-07-11 00:04:22 +0530

    docs: add usage examples

---
+++ examples/generate_cad_blueprint.py
@@ -0,0 +1,7 @@
+from src.contour_skeletonizer import ContourSkeletonizer
+
+def main):
+    skeletonizer = ContourSkeletonizer()
+    skeletonizer.generate_cad_blueprint('input_image.jpg')
+if __name__ == '__main__':
+    main()
